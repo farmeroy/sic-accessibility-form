30e608b856c3087d2afcce355d190af916f25069
Raffaele Cataldo <kemanicataldo@gmail.com>
2023-05-24 09:14:55 -0700

    forget about local storage

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import { useEffect } from "react";
 import { sections } from "./lib/list-config.json";
 import MainView from "./components/MainView";
 import { ListItem } from "./components/CheckList";
@@ -9,23 +8,6 @@ export interface ISection {
 }
 
 function App() {
-  useEffect(() => {
-    // we will store the values of each
-    // checkbox in local storage to access
-    // them at the end of the quiz
-    for (const section of sections) {
-      if (!localStorage.getItem(section.title)) {
-        localStorage.setItem(section.title, "[]");
-      }
-    }
-    return () => {
-      // remove all local storage items on reload
-      for (const section of sections) {
-        localStorage.removeItem(section.title);
-      }
-    };
-  });
-
   return (
     <div className="mx-auto max-h-vh bg-brandOrange">
       <MainView sections={sections} />

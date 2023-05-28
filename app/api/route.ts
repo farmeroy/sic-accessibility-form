3e98e672f3f279dd3565d631f9e5eddf2307ec56
Raffaele Cataldo <kemanicataldo@gmail.com>
2023-05-28 07:28:47 -0700

    basic send email endpoint WIP

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,11 +1,38 @@
 import { NextRequest } from "next/server";
+import nodemailer from "nodemailer";
 
 export async function POST(req: NextRequest) {
-  const res = await req.json();
-  console.log({ res });
-  const [name, email, message, results] = res;
+  const testAccount = await nodemailer.createTestAccount();
 
-  return new Response(res, {
+  const data = await req.json();
+
+  const { name, email, message, results } = data;
+
+  const transporter = nodemailer.createTransport({
+    host: "smtp.ethereal.email",
+    port: 587,
+    secure: false, // true for 465, false for other ports
+    auth: {
+      user: testAccount.user, // generated ethereal user
+      pass: testAccount.pass, // generated ethereal password
+    },
+  });
+  // send mail with defined transport object
+  const info = await transporter.sendMail({
+    from: `"${name}" <${email}>`, // sender address
+    to: "kemanicataldo@gmail.com", // list of receivers
+    subject: "Accessibility Form", // Subject line
+    text: message, // plain text body
+    html: `<b>${JSON.stringify(results)}</b>`, // html body
+  });
+  console.log("Message sent: %s", info.messageId);
+  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+  // Preview only available when sending through an Ethereal account
+  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+  // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+
+  return new Response("Sent", {
     status: 200,
     headers: {
       "Access-Control-Allow-Origin": "*",
